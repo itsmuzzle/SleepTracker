e9f349b5f35d33ec636085532a94ad712c58cf29
itsmuzzle <89t0asty@gmail.com>
2020-04-12 00:11:52 +0100

    Lesson 7.12

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepNightAdapter.kt
@@ -5,20 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.recyclerview.widget.ListAdapter
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepNight
 import convertDurationToFormatted
 import convertNumericQualityToString
 
-class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
 
     /**
      * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
@@ -46,13 +43,6 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
 
     /**
-     * Returns the total number of items in the data set held by the adapter.
-     *
-     * @return The total number of items in this adapter.
-     */
-    override fun getItemCount() = data.size
-
-    /**
      * Called by RecyclerView to display the data at the specified position. This method should
      * update the contents of the [ViewHolder.itemView] to reflect the item at the given
      * position.
@@ -74,7 +64,7 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
      * @param position The position of the item within the adapter's data set.
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         val res = holder.itemView.context.resources
         holder.bind(item, res)
     }
@@ -106,6 +96,63 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 return ViewHolder(view)
             }
         }
+    }
+
+    class SleepNightDiffCallback: DiffUtil.ItemCallback<SleepNight>() {
+        /**
+         * Called to check whether two objects represent the same item.
+         *
+         *
+         * For example, if your items have unique ids, this method should check their id equality.
+         *
+         *
+         * Note: `null` items in the list are assumed to be the same as another `null`
+         * item and are assumed to not be the same as a non-`null` item. This callback will
+         * not be invoked for either of those cases.
+         *
+         * @param oldItem The item in the old list.
+         * @param newItem The item in the new list.
+         * @return True if the two items represent the same object or false if they are different.
+         *
+         * @see Callback.areItemsTheSame
+         */
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem.nightId == newItem.nightId
+        }
+
+        /**
+         * Called to check whether two items have the same data.
+         *
+         *
+         * This information is used to detect if the contents of an item have changed.
+         *
+         *
+         * This method to check equality instead of [Object.equals] so that you can
+         * change its behavior depending on your UI.
+         *
+         *
+         * For example, if you are using DiffUtil with a
+         * [RecyclerView.Adapter], you should
+         * return whether the items' visual representations are the same.
+         *
+         *
+         * This method is called only if [.areItemsTheSame] returns `true` for
+         * these items.
+         *
+         *
+         * Note: Two `null` items are assumed to represent the same contents. This callback
+         * will not be invoked for this case.
+         *
+         * @param oldItem The item in the old list.
+         * @param newItem The item in the new list.
+         * @return True if the contents of the items are the same or false if they are different.
+         *
+         * @see Callback.areContentsTheSame
+         */
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
 }
--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -83,7 +83,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(this, Observer {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
